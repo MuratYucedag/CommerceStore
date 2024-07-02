e9eb8b917e42de43a33f8dfb7ead1ee437fa1d49
MuratYucedag <yucedagmurat23@gmail.com>
2024-07-02 16:52:51 +0300

    Add project files.

---
+++ Core/CommerceStore.Application/Features/CQRS/Handlers/RemoveCategoryCommadHandler.cs
@@ -0,0 +1,24 @@
+﻿using CommerceStore.Application.Features.CQRS.Commands;
+using CommerceStore.Application.Interfaces;
+using CommerceStore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommerceStore.Application.Features.CQRS.Handlers
+{
+    public class RemoveCategoryCommadHandler
+    {
+        private readonly IRepository<Category> _repository;
+        public RemoveCategoryCommadHandler(IRepository<Category> repository)
+        {
+            _repository = repository;
+        }
+        public async Task Handle(RemoveCategoryCommand command)
+        {
+            await _repository.DeleteAsync(command.Id);
+        }
+    }
+}
